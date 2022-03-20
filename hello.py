82984823c66511add30f2068180382daad5c759d
Volodymyr Vintila <vovavintila@gmail.com>
2022-03-20 17:21:03 +0100

    add templates, bootstrap, WTF-forms

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("What`s Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 # Create a route decorator
@@ -22,6 +32,18 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", user_name=name)
+
+
+# Create name page
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
 
 
 # Invalid URL
